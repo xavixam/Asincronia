b22ffd14b505f7d4378a35edd3b450eb7a35f7bb
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-10-23 13:12:57 +0200

    no message

--- extras.js
+++ extras.js
@@ -1,10 +1,31 @@
-let api = "https://dog.ceo/dog-api/"
+let breeds = "https://dog.ceo/api/breeds/list/all"
+let random = "https://dog.ceo/api/breeds/image/random"
 
 // Imprimir por consola la lista de razas de todos los perros.
-axios.get(api)
+axios.get(breeds)
 
 .then((res) => {
-    console.log(res);
+    for (const raza in res.data.message) {
+        console.log(raza);
+    }
 })
     
 .catch((err) => console.error(err))
+
+// Imprimir por consola la url imagen random de una raza.
+console.log(random);
+
+// Imprimir por consola todas las imágenes de una raza concreta.
+const divImages = document.querySelector("#divUsers")
+const elegido = prompt("Elige perro para mostrar imágenes:")
+
+axios.get(`https://dog.ceo/api/breed/${elegido}/images`)
+
+.then((res) => {    
+    res.data.message.forEach(element => {
+        divImages.innerHTML += `<div><img src="${element}" width="100px" height="100px" alt="Dog"></div>`
+    });
+})
+
+// *Extra ¿Y si ahora te pidiéramos que podamos poner otra raza en la url para que nos busque otras imágenes? 
+// Adapta las urls que ya tenías para que puedas pasarle argumentos.
